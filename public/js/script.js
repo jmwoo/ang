865e278a2954be304f9da3f02214e79ebe17bbd6
Jimmy Woolley <jamesmwoolley@gmail.com>
2014-10-18 14:11:17 -0600

    fixed angular controller register

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,4 @@ var MainCtrl = function ($scope) {
   $scope.posts = [{text: 'post-1'}, {text: 'post-2'}];
 };
 
-angular.module('app', [])
-  .controller('MainCtrl', ['$scope'], MainCtrl);
+angular.module('app', []).controller('MainCtrl', ['$scope', MainCtrl]);
